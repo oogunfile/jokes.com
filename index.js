f6cdfafdf2582ec43c36a4741206b139f04facee
seun_ogunfile <125077410+oogunfile@users.noreply.github.com>
2025-01-06 21:15:26 -0600

    iclude error handling

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ app.use(express.static("public"));
 
 //using the safe-mode options to ensure i dont get offesive or explicit jokes
 app.get("/", async(req,res)=>{
+    try{
     const response = await axios.get(baseUrl+"Any?safe-mode");
     const result = response.data;
     res.render("index.ejs", {jokeQuestion: result.setup, jokeAnswer: result.delivery});
-   
- 
+    }catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
 })
 app.listen(port, (req,res)=>{
     console.log(`server is running at port ${port}.`);
